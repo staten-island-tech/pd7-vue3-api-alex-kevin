ce428f8426a542cc3383518a4eee3ff9862254bc
AntiProSkills <113395660+gigaquantum@users.noreply.github.com>
2023-04-25 18:32:27 -0400

    Update ErrorView.vue

--- vue/src/views/ErrorView.vue
+++ vue/src/views/ErrorView.vue
@@ -1,17 +1,9 @@
 <script setup>
 import { RouterLink } from 'vue-router'
-import { computed } from 'vue'
-const props = defineProps({
-  errorCode: Number
-})
-
-/* const errorPath = computed(() => {
-  return `/error/${props.errorCode}`
-}) */
 </script>
 
 <template>
-  <h2>Error: {{ props.errorCode }}</h2>
+  <h2>Error: {{ $route.params.errorCode }}</h2>
   <p>It seems we've encountered an error.</p>
   <RouterLink to="/">
     <button>Return Home</button>
