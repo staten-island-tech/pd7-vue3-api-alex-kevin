38136a91cc92a9bca6b67a45b1ba0de2f260efd9
AntiProSkills <113395660+gigaquantum@users.noreply.github.com>
2023-04-25 13:09:05 -0400

    commit

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import StatisticsView from '../views/StatisticsView.vue'
 import SearchView from '../views/SearchView.vue'
+import ErrorView from '../views/ErrorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/search',
       name: 'Search',
       component: SearchView
+    },
+    {
+      path: '/error/:errorCode',
+      name: 'Error',
+      component: ErrorView
     }
   ]
 })
--- vue/src/views/ErrorView.vue
+++ vue/src/views/ErrorView.vue
@@ -1,14 +1,19 @@
 <script setup>
-import {RouterLink} from 'vue-router'
+import { RouterLink } from 'vue-router'
+import { computed } from 'vue'
 const props = defineProps({
-    errorCode: Number,
+  errorCode: Number
 })
+
+/* const errorPath = computed(() => {
+  return `/error/${props.errorCode}`
+}) */
 </script>
 
 <template>
-    <h2>Error: {{ errorCode }}</h2>
-    <p>It seems we've encountered and error.</p>
-      <RouterLink to="/">
-        <button>Return Home</button>
-      </RouterLink>
+  <h2>Error: {{ props.errorCode }}</h2>
+  <p>It seems we've encountered an error.</p>
+  <RouterLink to="/">
+    <button>Return Home</button>
+  </RouterLink>
 </template>
