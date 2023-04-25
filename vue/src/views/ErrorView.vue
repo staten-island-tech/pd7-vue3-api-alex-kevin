396782abeb53d756332792eac4abb95eb8893507
gigaquantum <113395660+gigaquantum@users.noreply.github.com>
2023-04-25 13:52:09 +0000

    created error page: url query not yet implemented

---
+++ vue/src/views/ErrorView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {RouterLink} from 'vue-router'
+const props = defineProps({
+    errorCode: Number,
+})
+</script>
+
+<template>
+    <h2>Error: {{ errorCode }}</h2>
+    <p>It seems we've encountered and error.</p>
+      <RouterLink to="/">
+        <button>Return Home</button>
+      </RouterLink>
+</template>
