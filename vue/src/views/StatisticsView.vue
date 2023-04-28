7fa7b369584de9de943eb48458fef13af2a27127
kevinc2465 <chenk5104@gmail.com>
2023-04-28 13:13:40 -0400

    working on the graph

--- vue/src/views/StatisticsView.vue
+++ vue/src/views/StatisticsView.vue
@@ -1,20 +1,31 @@
+<!-- <script src="https://cdn.jsdelivr.net/npm/chart.js"> -->
+
 <script setup>
-import { onMounted } from 'vue'
-async function getDataFromAPI(url) {
-  try {
-    let response = await fetch(url)
-  } catch (error) {
-    console.log(error)
-    alert(error)
+const ctx = document.getElementById('myChart')
+
+const chart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }
+    ]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
   }
-}
-onMounted(() => {
-  getDataFromAPI(
-    'https://data.cityofnewyork.us/resource/5ery-qagt.json?$query=SELECT%0A%20%20%60entityid%60%2C%0A%20%20%60schoolname%60%2C%0A%20%20%60number%60%2C%0A%20%20%60street%60%2C%0A%20%20%60city%60%2C%0A%20%20%60state%60%2C%0A%20%20%60borough%60%2C%0A%20%20%60zipcode%60%2C%0A%20%20%60lastinspection%60%2C%0A%20%20%60permittee%60%2C%0A%20%20%60inspectiondate%60%2C%0A%20%20%60ptet%60%2C%0A%20%2C20%60site_type%60%2C%0A%20%20%60level%60%2C%0A%20%20%60code%60%2C%0A%20%20%60violationdescription%60%%0A%20%20%60latitude%60%2C%0A%20%20%60longitude%60%2C%0A%20%20%60communityboard%60%2C%0A%20%20%60councildistrict%60%2C%0A%20%20%60censustract%60%2C%0A%20%20%60bin%60%2C%0A%20%20%60bbl%60%2C%0A%20%20%60nta%60%2C%0A%20%20%60borocode%60'
-  )
 })
 </script>
 
 <template>
   <p>statistics page</p>
+  <canvas id="myChart"></canvas>
 </template>
