18f176d9ae703767e0c66924deb28231db0165dd
kevinc2465 <chenk5104@gmail.com>
2023-04-30 23:15:00 -0400

    no

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+<<<<<<< HEAD
 
 import router from './router'
 
+=======
+import router from './router'
+
+>>>>>>> parent of 465be15 (trying to add another graph)
 import './assets/main.css'
 
 const app = createApp(App)
