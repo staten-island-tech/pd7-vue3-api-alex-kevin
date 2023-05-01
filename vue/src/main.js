7a719bf79753815230549fbd8864de7bf23f1468
kevinc2465 <chenk5104@gmail.com>
2023-04-30 23:20:55 -0400

    no

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,17 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-<<<<<<< HEAD
 
-import router from './router'
-
-=======
-import router from './router'
-
->>>>>>> parent of 465be15 (trying to add another graph)
 import './assets/main.css'
 
 const app = createApp(App)
 
-app.use()
+app.use(router)
 
 app.mount('#app')
