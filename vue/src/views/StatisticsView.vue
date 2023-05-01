6f2493d79d0e5b2011c06f5c493001c43228fdfe
kevinc2465 <chenk5104@gmail.com>
2023-05-01 13:02:29 -0400

    added the code I couldn't commit from home

--- vue/src/views/StatisticsView.vue
+++ vue/src/views/StatisticsView.vue
@@ -10,25 +10,23 @@ import {
   LinearScale
 } from 'chart.js'
 import { Bar } from 'vue-chartjs'
-import { Doughnut } from 'vue-chartjs'
+import Chart from 'chart.js/auto'
+
+
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 export default {
-  name: 'App',
+  name: 'Bar graph',
   components: {
-    Bar,
-    Doughnut
+    Bar
   },
   data() {
     return {
       loaded: false,
-      levelG: [],
-      levelC: [],
-      levelAVG: [],
       data: {
         labels: ['G', 'A', 'C'],
         datasets: [
           {
-            label: ['Amount of Schools'],
+            label: ['Amount of school'],
             backgroundColor: ['#85c9fa', '#1373d6', '#102e45'],
             borderColor: ['rgb(66, 191, 245)', 'rgb(15, 209, 255)', 'rgb(39, 196, 186)'],
             borderWidth: 4
@@ -40,25 +38,7 @@ export default {
       }
     }
   },
-  data() {
-    return {
-      loaded: false,
-      data: {
-        labels: ['G', 'A', 'C'],
-        datasets: [
-          {
-            label: ['Amount of Schools'],
-            backgroundColor: ['#85c9fa', '#1373d6', '#102e45'],
-            borderColor: ['rgb(66, 191, 245)', 'rgb(15, 209, 255)', 'rgb(39, 196, 186)'],
-            borderWidth: 4
-          }
-        ]
-      },
-      options: {
-        responsive: true
-      }
-    }
-  },
+
 
   async mounted() {
     try {
@@ -79,13 +59,41 @@ export default {
     } catch (error) {
       console.log(error)
     }
+  },
+  async mounted() {
+    console.log('Component mounted.')
+    const ctx = document.getElementById('myChart')
+
+
+    const data = {
+      labels: [
+        'No facilities available to sanitize utensils and equipments',
+        'Food not protected from contamination',
+        'Food protection certification not held by supervisor of food operations',
+        'Evidence of mice present in food'
+      ],
+      datasets: [
+        {
+          data: [300, 50, 100],
+          backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
+          hoverOffset: 4
+        }
+      ]
+    }
+
+
+    const myChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: data
+    }),
   }
 }
 </script>
 
 <template>
-  <h1>School Cleaniness Level</h1>
+  <h1>School Cleanliness Level</h1>
   <Bar v-if="loaded" :data="data" :options="options" />
+  <canvas id="myChart" width="400" height="400"></canvas>
 </template>
 
 <style scoped>
